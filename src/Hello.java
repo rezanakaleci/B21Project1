a5ea2ee580022180d93abfe0b0484abad3318cff
rezanakaleci <rezana.kaleci@gmail.com>
2022-04-23 11:45:09 +0200

    New comment is added

--- src/Hello.java
+++ src/Hello.java
@@ -2,6 +2,7 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello Cydeo");
         System.out.println("Starting the first project");
+        //new comment
 
 
     }
