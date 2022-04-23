7f28def76a8985f4dad07327511e77a976f04b75
rezanakaleci <rezana.kaleci@gmail.com>
2022-04-23 11:39:45 +0200

    Work 2 is done

--- src/Hello.java
+++ src/Hello.java
@@ -1,5 +1,8 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello Cydeo");
+        System.out.println("Starting the first project");
+
+
     }
 }
