689fe4869226b8cdf38c807becf17f37eefaaea5
rezanakaleci <rezana.kaleci@gmail.com>
2022-04-23 12:05:15 +0200

    comment 2 is added

--- src/Cocomelon.java
+++ src/Cocomelon.java
@@ -2,6 +2,7 @@ public class Cocomelon {
     public static void main(String[] args) {
         System.out.println("Cocomelon class is created");
        //have some practice
+        //making some exercises on java project
 
 
 
